d7511c8d8370e09b3433d3f895708ca8f2e0d35e
8688Add <42204083+8688Add@users.noreply.github.com>
2021-02-20 16:41:23 +0800

    Update and rename Lienol-Mtk.sh to Lienol-mtk.sh

--- Lienol-mtk.sh
+++ Lienol-mtk.sh
@@ -1,44 +1,22 @@
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
-#git clone https://github.com/Mattraks/helloworld.git package/lean/luci-app-ssr-plus
-svn co https://github.com/garypang13/openwrt-packages/trunk/smartdns package/smartdns
-git clone https://github.com/garypang13/luci-app-bypass.git package/luci-app-bypass
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
-git clone https://github.com/Lienol/openwrt-package.git package/openwrt-package
-#git clone https://github.com/8688Add/luci-app-vssr-plus.git package/luci-app-vssr-plus
-#git clone https://github.com/8688Add/luci-app-koolproxyR.git package/luci-app-koolproxyR
-git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
-#git clone https://github.com/frainzy1477/luci-app-clash.git package/luci-app-clash
-svn co https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom/trunk/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
-#git clone https://github.com/ElonH/Rclone-OpenWrt.git package/luci-app-rclone
-#git clone https://github.com/8688Add/luci-theme-opentomato.git package/luci-theme-opentomato
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
-#svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/luci-app-gost package/lean/luci-app-gost
-#svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/gost package/lean/gost
-#git clone https://github.com/riverscn/openwrt-iptvhelper.git package/luci-app-iptvhelper
-#git clone https://github.com/bin20088/luci-app-koolddns.git package/luci-app-koolddns
+rm -rf package/lean/luci-app-frpc
+git clone https://github.com/8688Add/luci-app-frpc-mod.git package/lean/luci-app-frpc
+chmod 0755 package/lean/luci-app-frpc/root/etc/init.d/frp
+rm -rf package/lean/luci-app-unblockmusic
+svn co https://github.com/project-openwrt/openwrt/trunk/package/lean/luci-app-unblockmusic package/lean/luci-app-unblockmusic
+git clone https://github.com/8688Add/luci-app-adbyby-plus-special.git package/luci-app-adbyby-plus-ram_edition-special
 
 #使用LEDE的ShadowSocksR Plus+出国软件 (源码自带passwall出国软件)
 svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/diy/luci-app-ssr-plus
 svn co https://github.com/fw876/helloworld/trunk/tcping package/diy/tcping
+svn co https://github.com/fw876/helloworld/trunk/xray-core package/diy/xray-core
 #svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/shadowsocksr-libev package/diy/shadowsocksr-libev
 #svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/pdnsd-alt package/diy/pdnsd-alt
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/microsocks package/diy/microsocks
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/dns2socks package/diy/dns2socks
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/simple-obfs package/diy/simple-obfs
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/v2ray-plugin package/diy/v2ray-plugin
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ipt2socks package/diy/ipt2socks
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/redsocks2 package/diy/redsocks2
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/dnsforwarder package/diy/dnsforwarder
-
-#赋予koolddns权限
-#chmod 0755 package/luci-app-koolddns/root/etc/init.d/koolddns
-#chmod 0755 package/luci-app-koolddns/root/usr/share/koolddns/aliddns
-
-# 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
-sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
 # 修改插件名字（修改名字后不知道会不会对插件功能有影响，自己多测试）
 #sed -i 's/"BaiduPCS Web"/"百度网盘"/g' package/lean/luci-app-baidupcs-web/luasrc/controller/baidupcs-web.lua
@@ -57,7 +35,3 @@ sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' package/lean/luci-app-sfe/p
 #sed -i 's/"Web 管理"/"Web"/g' package/lean/luci-app-webadmin/po/zh-cn/webadmin.po
 #sed -i 's/"管理权"/"改密码"/g' feeds/luci/modules/luci-base/po/zh-cn/base.po
 #sed -i 's/"带宽监控"/"监视"/g' feeds/luci/applications/luci-app-nlbwmon/po/zh-cn/nlbwmon.po
-
-#修改bypass的makefile
-find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
-find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
