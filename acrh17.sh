e79b8abecf04719d0c5acfee6d6e1784df1ac77f
8688Add <42204083+8688Add@users.noreply.github.com>
2021-03-16 18:58:48 +0800

    Update acrh17.sh

--- acrh17.sh
+++ acrh17.sh
@@ -43,7 +43,7 @@ rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 #git clone -b 18.06 https://github.com/garypang13/luci-theme-edge package/luci-theme-edge
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
+#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 
 #赋予koolddns权限
 #chmod 0755 package/luci-app-koolddns/root/etc/init.d/koolddns
