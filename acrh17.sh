2722d0f6af1a4ac2631a3d99e216f7937d9a32a4
8688Add <42204083+8688Add@users.noreply.github.com>
2021-03-17 16:40:43 +0800

    Update acrh17.sh

--- acrh17.sh
+++ acrh17.sh
@@ -18,7 +18,7 @@ git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 #svn co https://github.com/xiaorouji/openwrt-package/trunk/package package/lean/package
 #rm -rf package/lean/xray
 #svn co https://github.com/xiaorouji/openwrt-passwall/trunk/xray package/lean/xray
-svn co https://github.com/garypang13/openwrt-packages/trunk/smartdns package/smartdns
+svn co https://github.com/garypang13/openwrt-packages/trunk/smartdns-le package/smartdns-le
 git clone https://github.com/garypang13/luci-app-bypass.git package/luci-app-bypass
 git clone https://github.com/Lienol/openwrt-package.git package/openwrt-package
 #git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
